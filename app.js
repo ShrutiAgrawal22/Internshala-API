5e3c1aa5223d53afae4798d35b8151d76b1e8ba9
ShrutiAgrawal22 <115394677+ShrutiAgrawal22@users.noreply.github.com>
2024-04-20 11:42:24 +0530

    Basic Boiler Plate is Ready.

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("hey")
+require("dotenv").config({path: './.env'})     //requiring env file, to use port.
+
+const express = require('express');  //requiring express
+
+const app = express();    //saving express in app.
+
+//logger/morgan
+const logger = require('morgan');   //requiring morgan
+app.use(logger("tiny"));            //this will show on console that which route is hit.
+
+
+//Routes
+app.use("/", require('./routes/indexRoutes'));
+
+
+//so, this is our server set-up.
+app.listen(
+    process.env.PORT, 
+    console.log(`server running on port ${process.env.PORT}`)
+    )
+//bczz port is declared in the env file, so we are access it by : 
+//process.env.port and 
+//also requiring it at the top
+//so, this is our server set-up.
